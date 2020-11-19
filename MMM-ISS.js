bbe0c5a4b34cb8b686f9da82d565ec62b9115a40
Dave Terrell <5710124+daterrell@users.noreply.github.com>
2020-11-19 00:16:36 -0800

    Finished work, testing

--- MMM-ISS.js
+++ MMM-ISS.js
@@ -1,3 +1,10 @@
+/* Magic Mirror
+ * Module: MMM-ISS
+ *
+ * By Dave Terrell
+ * MIT Licensed.
+ */
+
 Module.register("MMM-ISS", {
   sightings: {},
   error: false,
@@ -9,103 +16,39 @@ Module.register("MMM-ISS", {
     minElevation: '40',
     country: 'United States',
     region: 'California',
-    city: 'Irvine',
-    header: 'ISS Sightings',
-    useHeader: false
+    city: 'Irvine'
   },
+
+  getStyles: function () {
+    return ["MMM-ISS.css", "bootstrap.min.css"];
+  },
+
+  getTemplate: function () {
+    return "MMM-ISS.njk"
+  },
+
+  getTemplateData: function () {
+    return this.sightings;
+  },
+
+  /**
+   * A sample item content:
+   * Date: Thursday Nov 19, 2020 <br/> Time: 7:12 PM <br/> Duration: less than 1 minute <br/> Maximum Elevation: 10° <br/> Approach: 10° above WNW <br/> Departure: 10° above WNW <br/> 
+   */
 
   getHeader: function () {
-    return this.config.useHeader && this.config.header;
+    return false;
   },
 
-  // Define required scripts.
-  getScripts: function () {
-  },
-
-  // set update interval
   start: function () {
     console.log("Starting up " + this.name);
     this.updateDom();
   },
 
   socketNotificationReceived: function (notification, payload) {
-    if (notification === "ERROR") {
-      this.error = true;
+    if (notification === "DATA_RESULT") {
+      this.sightings = payload;
+      this.updateDom(this.config.fadeSpeed);
     }
-    else if (notification !== "DATA_RESULT") {
-      return;
-    } else {
-      this.error = false;
-    }
-
-    this.sightings = payload;
-    this.updateDom(this.config.fadeSpeed);
-  },
-
-  // Update function
-  getDom: function () {
-    var wrapper = document.createElement("div");
-
-    var textWrapper = document.createElement("div");
-    textWrapper.className = "date normal medium";
-    textWrapper.innerHTML = this.config.event;
-    wrapper.appendChild(textWrapper);
-
-    var timeWrapper = document.createElement("div");
-    timeWrapper.className = "time bright xlarge light";
-    timeWrapper.style = "text-align: center;";
-
-    var target = moment(this.config.date).add(1, "day");
-    var now = moment();
-    var timeDiff = target.diff(now);
-
-    // Set days, hours, minutes and seconds
-    var diffDays = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-    var diffHours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var diffMinutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-    var diffSeconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
-
-    labelWrapper = function (text) {
-      var wrapper = document.createElement("sup");
-      wrapper.className = "dimmed";
-      wrapper.innerHTML = text;
-      return wrapper;
-    }
-
-    // Build the output
-    var days = document.createElement("div");
-    var hrs = document.createElement("div");
-    var tMinus = document.createElement("div");
-
-    if (diffDays <= -1) {
-      this.hide();
-      return wrapper;
-    }
-
-    if (diffDays < 0 && diffHours < 24) {
-      days.innerHTML = "TODAY!";
-      timeWrapper.appendChild(days);
-    } else {
-      if (diffDays > 1) {
-        days.innerHTML = diffDays;
-        days.appendChild(labelWrapper(this.config.daysLabel));
-      } else {
-        tMinus.innerHTML += `T-${this.pad(diffHours, 2)}:${this.pad(diffMinutes, 2)}:${this.pad(diffSeconds, 2)}`;
-      }
-    }
-
-    timeWrapper.appendChild(days);
-    timeWrapper.appendChild(hrs);
-    timeWrapper.appendChild(tMinus);
-
-    wrapper.appendChild(timeWrapper);
-
-    return wrapper;
-  },
-
-  pad: function pad(num, size) {
-    num = num.toString();
-    while (num.length < size) num = "0" + num;
-    return num;
   }
 });
--- node_helper.js
+++ node_helper.js
@@ -5,77 +5,124 @@
  * MIT Licensed.
  */
 
-var NodeHelper = require('node_helper')
+let NodeHelper = require('node_helper')
 const Parser = require('rss-parser');
 const Log = require("../../js/logger");
 
 module.exports = NodeHelper.create({
   feedUrl: template`https://spotthestation.nasa.gov/sightings/xml_files/${0}_${1}_${2}.xml`,
   requestInFlight: false,
-  
+
   getData: function () {
     if (this.requestInFlight) return;
 
-    var country = (this.config.country).replace(/ /g,"_");
-    var region = (this.config.region).replace(/ /g,"_");
-    var city = (this.config.city).replace(/ /g,"_");
-    var self = this;
+    let country = (this.config.country).replace(/ /g, "_");
+    let region = (this.config.region).replace(/ /g, "_");
+    let city = (this.config.city).replace(/ /g, "_");
+    let self = this;
 
-    var parser = new Parser();
+    let parser = new Parser();
     this.requestInFlight = true;
 
     parser.parseURL(feedUrl(country, region, city))
       .then(feed => {
-        var sightings = self.parseFeed(feed);
+        let sightings = self.parseFeed(feed);
         self.sendSocketNotification('DATA_RESULT', sightings);
       })
       .catch(err => {
         self.sendSocketNotification('ERROR', err);
       })
-      .finally(() =>  self.requestInFlight = false);
+      .finally(() => self.requestInFlight = false);
   },
 
   socketNotificationReceived: function (notification, payload) {
     if (notification === 'GET_DATA') {
-        this.getData()
+      this.getData()
     }
   },
 
-  parseFeed: function(feed) {
+  parseFeed: function (feed) {
     let offset = new Date().getTimezoneOffset();
     let plusMinus = offset >= 0 ? '-' : '+';
-    let stringOffset = "GMT" + plusMinus + ("0000" + (offset / 60 * 100)).substr(-4,4);
+    let stringOffset = "GMT" + plusMinus + ("0000" + (offset / 60 * 100)).substr(-4, 4);
     let now = new Date();
-    
-    let sightings = 
+
+    let sightings =
       feed.items
-        // Turn items' content from strings into JS object
         .map(item => {
+          /**
+           * A sample item content:
+           * Date: Thursday Nov 19, 2020 <br/> Time: 7:12 PM <br/> Duration: less than 1 minute <br/> Maximum Elevation: 10° <br/> Approach: 10° above WNW <br/> Departure: 10° above WNW <br/> 
+           */
           return item
             .content
             .split("<br/>")
             .filter(e => e.trim())
             .reduce((rv, el) => {
               let e = el.split(/:(.+)/); // Split string at first colon
-              rv[e[0].trim().replace(" ", "_")] = e[1].trim();
+
+              let prop = e[0].trim();
+              prop = prop.replace(" ", "_");
+
+              let data = e[1].trim();
+              data = data
+                .replace("less than ", "<")
+                .replace(" above ", "")
+                .replace("minute", "min");
+
+              rv[e[prop]] = data;
+
               return rv;
             }, {});
         })
         // Remove Date and Time properties, replace with proper DateTime object
         .map(sighting => {
           let newDateTime = new Date([sighting.Date, sighting.Time, stringOffset].join(' '));
+
           delete sighting.Date;
           delete sighting.Time;
+
           sighting.DateTime = newDateTime;
+
+          return sighting;
+        })
+        // Move directions (ex. WNW) out of Approach and Departure properties
+        .map(sighting => {
+          let splitParts = function (item) {
+            let parts = item.split("°");
+
+            return {
+              degree: parts[0],
+              direction: parts[1]
+            };
+          };
+
+          let app = splitParts(sightings.Approach);
+          sighting.Approach = app.degree;
+          sighting.Approach_Direction = app.direction;
+
+          let dep = splitParts(sighting.Departure);
+          sighting.Departure = dep.degree;
+          sighting.Departure_Direction = dep.direction;
+
           return sighting;
         })
         // Remove sightings that are in the past
         .filter(sighting => sighting.DateTime >= now)
         // Remove sightings that are too low in the sky 
-        .filter(sighting => parseInt(sighting.Maximum_Elevation) >= 40)
+        .filter(sighting => parseInt(sighting.Maximum_Elevation) >= this.config.minElevation)
         // Ensure we're listed by date
-        .sort((a, b) => a.DateTime - b.DateTime);
+        .sort((a, b) => a.DateTime - b.DateTime)[0];
 
-    return sightings;
+    return {
+      date: sightings.DateTime.toLocaleDateString([], { weekday: "short", month: "short", day: "numeric" }),
+      time: sightings.DateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+      visible: sighting.Duration,
+      maxHeight: sightings.Maximum_Elevation,
+      appears: sightings.Approach,
+      appearsDirection: sightings.Approach_Direction,
+      disappears: sightings.Departure,
+      disappearsDirection: sightings.Departure_Direction,
+    };
   }
 });
